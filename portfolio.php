88ff373310381d067e71599ae4b2e1aa083ae6e7
Aqib1928 <aqib21028@gmail.com>
2025-09-24 01:31:45 +0500

    changes

--- portfolio.php
+++ portfolio.php
@@ -49,71 +49,29 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-4 col-md-4 col-12 custom-grid BranDing" data-wow-duration="2000ms">
+                            <div class="col-lg-3 col-md-4 col-12 custom-grid wordPress" data-wow-duration="2000ms">
                                 <div class="">
                                     <div class="wpo-protfolio-single">
                                         <div class="wpo-protfolio-img">
-                                            <img src="assets/images/protfolio/img-6.jpg" alt="">
+                                            <img src="assets/images/project/A1DumsterRental.png" alt="">
                                         </div>
                                         <div class="wpo-protfolio-text">
-                                            <h2>Abstract Art</h2>
-                                            <span>Illustration . Art Direction</span>
+                                            <h3>A1 Dumpster Rental</h3>
+                                            <span>Wilmington, Delaware</span>
                                             <a href="portfolio-single.php">View Work</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-4 col-md-4 col-12 custom-grid  Marketing" data-wow-duration="2000ms">
+                            <div class="col-lg-6 col-md-4 col-12 custom-grid wordPress" data-wow-duration="2000ms">
                                 <div class="">
                                     <div class="wpo-protfolio-single">
                                         <div class="wpo-protfolio-img">
-                                            <img src="assets/images/protfolio/img-7.jpg" alt="">
+                                            <img src="assets/images/project/A1DumsterRental.png" alt="">
                                         </div>
                                         <div class="wpo-protfolio-text">
-                                            <h2>3D Project</h2>
-                                            <span>Illustration . Art Direction</span>
-                                            <a href="portfolio-single.php">View Work</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-md-4 col-12 custom-grid 3d" data-wow-duration="2000ms">
-                                <div class="">
-                                    <div class="wpo-protfolio-single">
-                                        <div class="wpo-protfolio-img">
-                                            <img src="assets/images/protfolio/img-8.jpg" alt="">
-                                        </div>
-                                        <div class="wpo-protfolio-text">
-                                            <h2>Modern BG</h2>
-                                            <span>Illustration . Art Direction</span>
-                                            <a href="portfolio-single.php">View Work</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-md-4 col-12 custom-grid zoomIn" data-wow-duration="2000ms">
-                                <div class="">
-                                    <div class="wpo-protfolio-single">
-                                        <div class="wpo-protfolio-img">
-                                            <img src="assets/images/protfolio/img-5.jpg" alt="">
-                                        </div>
-                                        <div class="wpo-protfolio-text">
-                                            <h2>Product Design</h2>
-                                            <span>Illustration . Art Direction</span>
-                                            <a href="portfolio-single.php">View Work</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-md-4 col-12 custom-grid wow" data-wow-duration="2000ms">
-                                <div class="">
-                                    <div class="wpo-protfolio-single">
-                                        <div class="wpo-protfolio-img">
-                                            <img src="assets/images/protfolio/img-4.jpg" alt="">
-                                        </div>
-                                        <div class="wpo-protfolio-text">
-                                            <h2>Web Design</h2>
-                                            <span>Illustration . Art Direction</span>
+                                            <h3>A1 Dumpster Rental</h3>
+                                            <span>Wilmington, Delaware</span>
                                             <a href="portfolio-single.php">View Work</a>
                                         </div>
                                     </div>
